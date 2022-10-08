caaf0e5d3ea3bb898e4621dac8d98e6a97530010
KGouthamReddy <kasireddygouthamreddy74135@gmail.com>
2022-10-08 11:39:59 +0530

    chjamges

---
+++ s01Springboothelloworld/src/main/java/com/example/demo/S01SpringboothelloworldApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S01SpringboothelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S01SpringboothelloworldApplication.class, args);
+		System.out.println("hello world");
+	}
+
+}
